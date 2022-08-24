212ea3a98d5a35b839cbafefc4a9fc3f40beaa08
graceannobrien <graceo@metrumrg.com>
2022-08-24 10:29:41 -0400

    push to master

--- tests/testthat/test-nmvalidate.R
+++ tests/testthat/test-nmvalidate.R
@@ -1,4 +1,6 @@
 
+n_tests <- length(nm_validate(nm, nm_spec))
+
 # Check no failures
 test_that("Works with no failures", {
   x = nm_validate(nm, nm_spec)
@@ -6,41 +8,39 @@ test_that("Works with no failures", {
   all(purrr::map_lgl(x, ~ .x$success)))
 })
 
-# Check failure on test 1: no duplicates across id, tafd, and primary keys
+# Check failure on each test
 test_that("Works with failure on test 1: no duplicates across id, tafd, and primary keys", {
   x <- nm %>%
-    # rep 1 row
+    slice(rep(1,2)) %>%
     nm_validate(nm_spec)
   expect_equal(
     sum(purrr::map_lgl(x, ~ .x$success)),
-    length(x) - 2
+    length(x) - 1
   )
   expect_false(x[[1]]$success)
   expect_true(nrow(x[[1]]$error_content) == 2)
   expect_true(any(grepl(
-    "no duplicates across id, tafd, and primary keys",
+    "No duplicates across id, tafd, and primary keys", #make lower case to match
     x[[1]]$description
   )))
 })
 
-# Check failure on test 2: no duplicate subject level covariates
-test_that("Works with failure on test 1: no duplicate subject level covariates", {
+test_that("Works with failure on test 1: within subject, no differing subject level covariates", {
   x <- nm %>%
-   # make dup subj level covar
+    mutate(AGEBL = replace(AGEBL, ID == 4 & NUM == 68, 52)) %>%
     nm_validate(nm_spec)
   expect_equal(
     sum(purrr::map_lgl(x, ~ .x$success)),
-    length(x) - 2
+    length(x) - 1
   )
   expect_false(x[[2]]$success)
   expect_true(nrow(x[[2]]$error_content) == 2)
   expect_true(any(grepl(
-    "no duplicates across id, tafd, and primary keys",
+    "no duplicate subject level covariates",
     x[[2]]$description
   )))
 })
 
-# Check failure on test 3: no na covariates
 test_that("Works with failure on test 3: no na covariates", {
   x <- nm %>%
     mutate(AGEBL = replace(AGEBL, ID == 4, NA)) %>%
@@ -50,26 +50,54 @@ test_that("Works with failure on test 3: no na covariates", {
     length(x) - 1
   )
   expect_false(x[[3]]$success)
-  expect_true(nrow(x[[3]]$error_content) == 27) # want 1- fix with x filter
+  expect_true(nrow(x[[3]]$error_content) == 27)
   expect_true(any(grepl(
     "no na covariates",
     x[[3]]$description
   )))
 })
 
-# Check failure on test 4: similar continuous covariates across studies
-test_that("Error content output", {
+test_that("Works with failure on test 4: similar continuous covariates across studies", {
   x <- nm %>%
-    mutate(mess up one column) %>%
+    mutate(
+      AGEBL = case_when(
+        STUDYID == "STUDY-X" ~ AGEBL*0.5,
+        TRUE ~ AGEBL
+      )
+    ) %>%
     nm_validate(nm_spec)
   expect_equal(
     sum(purrr::map_lgl(x, ~ .x$success)),
     length(x) - 1
   )
-  expect_false(x[[3]]$success)
-  expect_true(nrow(x[[3]]$error_content) == 1)
-  #expect_true(anygrepl for description
+  expect_false(x[[4]]$success)
+  expect_true(nrow(x[[4]]$error_content) == 2)
+  expect_true(any(grepl(
+    "similar continuous covariates across studies",
+    x[[4]]$description
+  )))
 })
+
+# test_that("Works with failure on test 4: similar continuous covariates across studies", {
+#   x <- nm %>%
+#     mutate(
+#       AGEBL = case_when(
+#         STUDYID == "STUDY-X" ~ AGEBL*0.7,
+#         TRUE ~ AGEBL
+#       )
+#     ) %>%
+#     nm_validate(nm_spec)
+#   expect_equal(
+#     sum(purrr::map_lgl(x, ~ .x$success)),
+#     length(x) - 1
+#   )
+#   expect_false(x[[4]]$success)
+#   expect_true(nrow(x[[4]]$error_content) == 2)
+#   expect_true(any(grepl(
+#     "similar continuous covariates across studies",
+#     x[[4]]$description
+#   )))
+# })
 
 # Check print method for no failures
 test_that("Works with no failures: print method", {
@@ -82,7 +110,7 @@ test_that("Works with no failures: print method", {
   print(nm_validate(nm, nm_spec))
   res <- readLines(.f)
   expect_true(any(grepl(
-    "4 of 4 checks PASSED",
+    glue::glue("{n_tests} of {n_tests} checks PASSED"),
     res
   )))
 })
@@ -98,7 +126,7 @@ test_that("Works with some failures: print method", {
   capture.output(print(nm_validate(nm_errors, nm_spec)))
   res <- readLines(.f)
   expect_true(any(grepl(
-    "1 of 4 checks PASSED",
+    glue::glue("1 of {n_tests} checks PASSED"),
     res
   )))
 })
