bfafcca81d1748f48f70329712059b5cadf0c9d4
michaelmcd18 <michaelm@metrumrg.com>
2022-12-07 10:55:55 -0500

    fix format of stories yaml

--- inst/validation/build-validation-docs.R
+++ inst/validation/build-validation-docs.R
@@ -12,7 +12,7 @@
 #######################################################
 
 PKGNAME <- "mrgda"
-PKGVERSION <- "0.2.0"
+PKGVERSION <- "0.2.1"
 STYLE_REF_DIR <- "docx-ref-header-image" # set to NULL if not using style ref
 
 # set up directories and clear existing output dirs, if they exist
