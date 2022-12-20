5556acadac1bc942d0733c3d7f5392596b6edb40
andersone1 <andersone@metrumrg.com>
2022-12-20 15:47:37 -0500

    Update test-mutate-id.R

--- tests/testthat/test-mutate-id.R
+++ tests/testthat/test-mutate-id.R
@@ -3,13 +3,13 @@ test_df <- dplyr::tibble(
      WT = c(64, 23, 92),
      SEX = c(1, 2, 1))
 
-test_that("mutate_id creates appropriate ID's for standard case [NMV-MID-001]", {
+test_that("mutate_id creates appropriate IDs for standard case [NMV-MID-001]", {
   test_id <- test_df %>% mutate_id(.unique_subject_identifier = USUBJID, .start_id = 1)
   expect_equal(test_id$ID[1], 1)
   expect_equal(test_id$ID[3], 3)
 })
 
-test_that("mutate_id creates appropriate ID's with unique starting ID [NMV-MID-002]", {
+test_that("mutate_id creates appropriate IDs with unique starting ID [NMV-MID-002]", {
   test_id <- test_df %>% mutate_id(.unique_subject_identifier = USUBJID, .start_id = 105)
   expect_equal(test_id$ID[1], 105)
   expect_equal(test_id$ID[3], 107)
