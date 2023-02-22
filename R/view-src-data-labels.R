1eb915bf18f141ebfef98eaf36b53d87f067806d
michaelmcd18 <michaelm@metrumrg.com>
2023-02-22 13:16:56 -0500

    add src data viewer function

---
+++ R/view-src-data-labels.R
@@ -0,0 +1,54 @@
+#' View source data domains and column labels
+#'
+#' @description
+#' After loading in your source data, use `view_src_data_labels()` to view the
+#' description of each domain and column.
+#'
+#' @param .src_list a named list with each source data domain (ideally use the output of `read_src_dir()`)
+#'
+#' @examples
+#' path <- system.file("example-sdtm", package = "mrgda")
+#' # Read in all source files
+#' src_list <- read_src_dir(.path = path, .file_types = "detect")
+#'
+#' view_src_data_labels(.src_list = src_list)
+#'
+#' @export
+view_src_data_labels <- function(.src_list) {
+
+  src_summary <- dplyr::tibble()
+
+  for (domain.i in names(.src_list)) {
+
+    .domain_name <- domain.i
+
+    if (is.null(attr(.src_list[[domain.i]], "label"))) {
+      .domain_desc <- "Missing"
+    } else {
+      .domain_desc <- attr(.src_list[[domain.i]], "label")
+    }
+
+    for (column.i in names(.src_list[[domain.i]])) {
+
+      .column_name <- column.i
+
+      if (is.null(attr(.src_list[[domain.i]], "label"))) {
+        .column_desc <- "Missing"
+      } else {
+        .column_desc <- attr(.src_list[[domain.i]][[column.i]], "label")
+      }
+
+      temp_df <- dplyr::tibble(
+        DOMAIN_NAME = .domain_name,
+        DOMAIN_LABEL = .domain_desc,
+        COLUMN_NAME = .column_name,
+        COLUN_LABEL = .column_desc
+      )
+
+      src_summary <- dplyr::bind_rows(src_summary, temp_df)
+
+    }
+  }
+
+  src_summary %>% tibble::view("SourceDataLabels")
+}
