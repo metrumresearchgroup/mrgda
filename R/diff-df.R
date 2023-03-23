58e70c3efc942c287f62c64a415dffb7cdec6de5
graceo <graceo@metrumrg.com>
2023-03-23 16:42:51 -0400

    diff df comments

--- R/diff-df.R
+++ R/diff-df.R
@@ -7,3 +7,24 @@ diff_df <- function(.base, .compare, .file){
     suppress_warnings = TRUE
   )
 }
+
+
+# This blows up in certain cases, e.g.:
+
+# x=  mtcars %>% arrange(cyl,mpg,hp)
+
+# y = bind_rows(mtcars, slice(mtcars,1) %>% arrange(cyl,mpg,hp))
+
+
+#
+# added_rows <-  inner_join(x,y) == x
+# dropped_rows
+
+# added rows
+# dropped rows
+# added column
+# dropped columns
+# changed columns (check every column)
+
+# old <- readr::read_csv(system.file("derived", "pk.csv", package = "mrgda"), na = ".")
+# new <= old
