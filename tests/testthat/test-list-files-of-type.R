ce9ab80afd26ce4731246bd171b8e833270274a6
andersone <andersone@metrumrg.com>
2023-03-23 12:35:37 -0400

    prep for release

--- tests/testthat/test-list-files-of-type.R
+++ tests/testthat/test-list-files-of-type.R
@@ -1,14 +1,14 @@
 path <- system.file("example-sdtm", package = "mrgda")
 
 
-test_that("list_files_of_type: check detect works", {
+test_that("list_files_of_type: check detect works in test dir [LFOT-001]", {
   expect_equal(list_files_of_type(path, "detect")$type, "xpt")
 })
 
-test_that("list_files_of_type: check xpt works", {
+test_that("list_files_of_type: check xpt works in test dir [LFOT-002]", {
   expect_equal(basename(list_files_of_type(path, "xpt")$files_of_type[1]), "ae.xpt")
 })
 
-test_that("list_files_of_type: check sas7bdat does not work", {
+test_that("list_files_of_type: check sas7bdat does not work in test dir [LFOT-003]", {
   expect_equal(length(list_files_of_type(path, "sas7bdat")$files_of_type), 0)
 })
