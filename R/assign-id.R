6b8594a0f48693b52448aa0bd11a028c78398a05
michaelmcd18 <michaelm@metrumrg.com>
2023-05-31 12:50:55 -0400

    update get_data_version and assign_id

--- R/assign-id.R
+++ R/assign-id.R
@@ -1,27 +1,17 @@
-#' Subject ID Assignment Tracking
+#' Assign Subject IDs (creates an ID column)
 #'
 #' @description
 #' Upon first use, an ID column will be mutated to the given data set according
-#' to the specified subject column. In addition, an ID lookup csv file will be
-#' created containing the assigned ID for each subject.
+#' to the specified subject column.
 #'
-#' For each future use, ID's will be assigned according to the lookup csv file. If
-#' new subjects are added to the data, the lookup file will be modified to include
-#' the new subjects to assign them each a unique identifier.
+#' For each subsequent use, IDs will be assigned to match the previous derived data
+#' set. If new subjects are found in the data, they will be assigned a
+#' unique value not present in the previous derived data (starting at the maximum
+#' of the previous IDs plus 1).
 #'
 #' @param .data A data.frame to have ID column mutated onto.
-#' @param .lookup_file A string containing the file path of the ID lookup csv file
+#' @param .previously_derived_path A string containing the file path of the previous derived data set
 #' @param .subject_col A character string containing the column name of the subject identifying column in the data
-#'
-#' @details
-#' This function creates a lookup for subject IDs. If an ID column already exists in the data,
-#' or the specified subject column does not exist the function will stop.
-#' If a lookup file does not exist at the specified path, the function
-#' will create a new lookup and write it to the provided path. If a lookup file does exist,
-#' the function will assign ID to the data according to the lookup.
-#'
-#' If subjects exist in the data but not in the lookup, they will be added to the lookup
-#' and assigned an unique ID value.
 #'
 #' @examples
 #' Theoph2 <-
@@ -32,14 +22,11 @@
 #' df_with_id <-
 #'   assign_id(
 #'     Theoph2,
-#'     .lookup_file = paste0(tempfile(), ".csv"),
 #'     .subject_col = "USUBJID"
 #'   )
 #'
-#' @return A data frame with an additional ID column, which contains the numeric IDs for each subject in the subject column.
-#'
 #' @export
-assign_id <- function(.data, .lookup_file, .subject_col = "USUBJID") {
+assign_id <- function(.data, .previously_derived_path = NULL, .subject_col = "USUBJID") {
 
   if (!is.null(.data[["ID"]])) {
     stop("Data already contains ID")
@@ -49,33 +36,52 @@ assign_id <- function(.data, .lookup_file, .subject_col = "USUBJID") {
     stop("Subject column not found in data")
   }
 
-  if(!file.exists(.lookup_file)) {
+  make_id <- function(.data, .subject_col, .to_add = 0){
 
-    id_lookup <-
+    .data_with_id <-
       .data %>%
       dplyr::mutate(
-        ID =  as.numeric(forcats::fct_inorder(!!sym(.subject_col)))
-      ) %>%
-      dplyr::select(dplyr::all_of(c("ID", .subject_col))) %>%
-      dplyr::distinct()
+        ID = as.numeric(forcats::fct_inorder(!!sym(.subject_col))) + .to_add
+      )
 
-    stopifnot(is_unique_by_subject(id_lookup, .subject_col))
-    stopifnot(is_unique_by_subject(id_lookup, "ID"))
-
-    readr::write_csv(x = id_lookup, file = .lookup_file)
+    .count_id <- .data_with_id %>% dplyr::distinct(ID)
 
     print(
       cli::boxx(
-        header = "New ID Lookup Created",
-        label = c(paste0("Number of subjects in lookup: ", nrow(id_lookup)),
-                  paste0("Lookup file written to: ", .lookup_file)))
+        header = "New IDs Assigned",
+        label = c(paste0("Number of subjects assigned: ", nrow(.count_id))))
     )
 
-  } else {
-    id_lookup <- readr::read_csv(.lookup_file) %>% suppressMessages()
+    return(.data_with_id)
   }
 
+  # Assigns IDs from scratch if no path
+  if(is.null(.previously_derived_path)){
+    return(make_id(.data, .subject_col))
+  }
+
+  # Assigns IDs from scratch if no previous file
+  if(!file.exists(.previously_derived_path)){
+    return(make_id(.data, .subject_col))
+  }
+
+  previous_data <- readr::read_csv(.previously_derived_path) %>% suppressMessages()
+
+  if (is.null(previous_data[["ID"]])) {
+    stop("ID column not found in previous data ")
+  }
+
+  if (is.null(previous_data[[.subject_col]])) {
+    stop("Subject column not found in previous data ")
+  }
+
+  id_lookup <-
+    previous_data %>%
+    dplyr::select(dplyr::all_of(c("ID", .subject_col))) %>%
+    dplyr::distinct()
+
   stopifnot(!anyNA(id_lookup))
+
   new_id_check <- !all(.data[[.subject_col]] %in% id_lookup[[.subject_col]])
 
   # New subjects coming into existing data set
@@ -86,32 +92,20 @@ assign_id <- function(.data, .lookup_file, .subject_col = "USUBJID") {
       dplyr::left_join(id_lookup) %>%
       dplyr::filter(is.na(ID)) %>%
       dplyr::distinct(!!sym(.subject_col)) %>%
-      dplyr::mutate(
-        ID =  as.numeric(forcats::fct_inorder(!!sym(.subject_col))) + max(id_lookup$ID, na.rm = TRUE)
-      )
+      make_id(.data = .,
+              .subject_col = .subject_col,
+              .to_add = max(id_lookup$ID, na.rm = TRUE))
 
-    id_lookup <-
-      id_lookup %>%
-      dplyr::bind_rows(missing_ids)
+    id_lookup <- id_lookup %>% dplyr::bind_rows(missing_ids)
 
     stopifnot(is_unique_by_subject(id_lookup, .subject_col))
     stopifnot(is_unique_by_subject(id_lookup, "ID"))
 
-    readr::write_csv(id_lookup, file = .lookup_file)
-
-    id_subj_combo <-
-      missing_ids %>%
-      dplyr::mutate(
-        SUBJCOL = paste0(ID, " - ", !!sym(.subject_col))
-      )
-
+  } else {
     print(
       cli::boxx(
-        header = "New Subjects Added to ID Lookup",
-        label = c(paste0("Number of new subjects added to lookup: ", nrow(missing_ids)),
-                  paste0("New ID: ", id_subj_combo$SUBJCOL)))
+        label = "No new IDs assigned")
     )
-
   }
 
   .data_w_id <-
--- tests/testthat/test-assign-id.R
+++ tests/testthat/test-assign-id.R
@@ -2,66 +2,71 @@ test_that("assign_id function works correctly [NMV-AID-001]", {
 
   # Generate a sample dataframe for the tests
   .data <- data.frame(USUBJID = c("A", "B", "C"))
-  .lookup_file <- paste0(tempfile(), ".csv")
+  .new_data_file <- paste0(tempfile(), ".csv")
 
-  expect_true(nrow(assign_id(.data, .lookup_file = .lookup_file)) == 3) %>% suppressMessages()
-  expect_true(ncol(assign_id(.data, .lookup_file = .lookup_file)) == 2) %>% suppressMessages()
+  expect_true(nrow(assign_id(.data, .previously_derived_path = .new_data_file)) == 3) %>% suppressMessages()
+  expect_true(ncol(assign_id(.data, .previously_derived_path = .new_data_file)) == 2) %>% suppressMessages()
+
+  expect_true(nrow(assign_id(.data)) == 3) %>% suppressMessages()
+  expect_true(ncol(assign_id(.data)) == 2) %>% suppressMessages()
+
+  write.csv(assign_id(.data), .new_data_file)
+
+
+  expect_true(nrow(assign_id(.data, .previously_derived_path = .new_data_file)) == 3) %>% suppressMessages()
+  expect_true(ncol(assign_id(.data, .previously_derived_path = .new_data_file)) == 2) %>% suppressMessages()
 
   # Test 1: Testing whether the function runs without error
   test_that("Function runs without error", {
-    expect_error(assign_id(.data, .lookup_file, .subject_col = "USUBJID"), NA) %>% suppressMessages()
+    expect_error(assign_id(.data, .new_data_file, .subject_col = "USUBJID"), NA) %>% suppressMessages()
   })
 
   # Test 2: Testing the function when the "ID" column already exists
   .data_with_id <- .data
   .data_with_id$ID <- c(1, 2, 3)
   test_that("Function correctly detects presence of 'ID' column", {
-    expect_error(assign_id(.data_with_id, .lookup_file, .subject_col = "USUBJID"), "Data already contains ID") %>% suppressMessages()
+    expect_error(assign_id(.data_with_id, .new_data_file, .subject_col = "USUBJID"), "Data already contains ID") %>% suppressMessages()
   })
 
   # Test 3: Testing the function when the subject column doesn't exist
   .data_without_subject <- .data
   names(.data_without_subject)[1] <- "SUBJ"
   test_that("Function correctly detects absence of subject column", {
-    expect_error(assign_id(.data_without_subject, .lookup_file, .subject_col = "USUBJID"), "Subject column not found in data")
-  })
-
-  # Test 4: Testing that the function correctly creates an "ID" column and writes a csv file
-  test_that("Function creates 'ID' column and writes csv file", {
-    assign_id(.data, .lookup_file, .subject_col = "USUBJID") %>% suppressMessages()
-    expect_true(file.exists(.lookup_file))
-    lookup_data <- readr::read_csv(.lookup_file) %>% suppressMessages()
-    expect_equal(colnames(lookup_data), c("ID", "USUBJID"))
+    expect_error(assign_id(.data_without_subject, .new_data_file, .subject_col = "USUBJID"), "Subject column not found in data")
   })
 
   # Test 5: Testing that the function adds new IDs correctly
   .data_new_subject <- data.frame(USUBJID = c("A", "B", "C", "D", "E"))
   test_that("Function correctly adds new subjects", {
-    df_with_new_ids <- assign_id(.data_new_subject, .lookup_file, .subject_col = "USUBJID") %>% suppressMessages()
+    df_with_new_ids <- assign_id(.data_new_subject, .new_data_file, .subject_col = "USUBJID") %>% suppressMessages()
     expect_equal(df_with_new_ids$ID, c(1, 2, 3, 4, 5))
   })
+
+
+  test_that("assign_id adds new subjects and provides unique ID", {
+
+    # Generate a sample dataframe for the tests
+    data <- data.frame(USUBJID = c("A", "B", "C", "D"))
+    lookup_path <- paste0(tempfile(), ".csv")
+
+    data_w_id <-
+      assign_id(.data = data, .new_data_file, .subject_col = "USUBJID") %>% suppressMessages()
+
+    expect_equal(ncol(data_w_id), 2)
+    expect_equal(nrow(data_w_id), 4)
+    expect_true(c("ID") %in% names(data_w_id))
+    expect_true(c("USUBJID") %in% names(data_w_id))
+
+    data <- data.frame(USUBJID = c("A", "B", "G", "D", "E", "F", "C"))
+
+    data_w_id2 <-
+      assign_id(.data = data, .new_data_file, .subject_col = "USUBJID") %>% suppressMessages()
+
+    expect_true(data_w_id2$ID[data_w_id2$USUBJID == "C"] == 3)
+    expect_true(nrow(data_w_id2) == 7)
+
+  })
+
 })
 
-test_that("assign_id adds new subjects and provides unique ID", {
 
-  # Generate a sample dataframe for the tests
-  data <- data.frame(USUBJID = c("A", "B", "C", "D"))
-  lookup_path <- paste0(tempfile(), ".csv")
-
-  data_w_id <-
-    assign_id(.data = data, .lookup_file = lookup_path, .subject_col = "USUBJID") %>% suppressMessages()
-
-  expect_equal(ncol(data_w_id), 2)
-  expect_equal(nrow(data_w_id), 4)
-  expect_true(c("ID") %in% names(data_w_id))
-  expect_true(c("USUBJID") %in% names(data_w_id))
-
-  data <- data.frame(USUBJID = c("A", "B", "G", "D", "E", "F", "C"))
-
-  data_w_id2 <-
-    assign_id(.data = data, .lookup_file = lookup_path, .subject_col = "USUBJID") %>% suppressMessages()
-
-  expect_true(data_w_id2$ID[data_w_id2$USUBJID == "C"] == 3)
-  expect_true(nrow(data_w_id2) == 7)
-
-})
