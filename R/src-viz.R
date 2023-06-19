9856ce9adea336b8106b6a105bd9e5c320c785b2
Eric Anderson <ericanderson@andersonesMBP16.lan>
2023-06-19 14:27:34 -0400

    src viz

--- R/src-viz.R
+++ R/src-viz.R
@@ -1,246 +1,160 @@
-#' src_viz
+#' src_viz function
 #'
-#' This function provides a Shiny app for visualizing source data organized by different domains and subjects.
+#' This function launches a Shiny app that allows a user to visualize a list of data frames.
+#' The user can select a data frame from the list to view, filter it based on the 'USUBJID' column,
+#' and color the rows alternately based on the values in the 'USUBJID' column.
+#' The 'label' attribute of each column, if present, is displayed under the column name.
 #'
-#' @param .src_list A named list of data.frames where each data.frame represents a different domain of source data.
-#' The names of the list items should correspond to the domain names. One of the list items should be named "mrgda_labels"
-#' and contain column labels for the various domains.
+#' @param .src_list A named list of data frames to be visualized.
 #'
-#' The Shiny app provided by this function consists of a dashboard with two main sections: "Domains" and "Subject Drilldown".
-#' In the "Domains" section, users can select a domain from the provided list and view related summary information and data.
-#' In the "Subject Drilldown" section, users can enter a subject ID and choose the domains they want to display. The selected
-#' domains' data related to the entered subject ID will be shown.
+#' @return None. The function launches a Shiny app.
+#' @export
 #'
-#' @return A Shiny app for visualizing the source data.
 #' @examples
-#' \dontrun{
 #' read_src_dir(here::here("inst", "example-sdtm")) %>% src_viz()
-#' }
-#' @internal
-src_viz <- function(.src_list){
-
-  domain_names <- names(.src_list)[!grepl("mrgda", names(.src_list), fixed=TRUE)]
-
-  ui <-
-    shinydashboard::dashboardPage(
-      shinydashboard::dashboardHeader(titleWidth = 200),
-      shinydashboard::dashboardSidebar(
-        width = 200,
-        shinydashboard::sidebarMenu(
-          shinydashboard::menuItem("Domains", tabName = "domains", icon = shiny::icon("table")),
-          shinydashboard::menuItem("Subject Drilldown", tabName = "subjects", icon = shiny::icon("person"))
-        )
-      ),
-      shinydashboard::dashboardBody(
-        shinydashboard::tabItems(
-          shinydashboard::tabItem(
-            tabName = "domains",
-            shiny::fluidRow(
-              shiny::column(
-                width = 3,
-                shinydashboard::box(
-                  title = "Domain Selection",
-                  solidHeader = TRUE,
-                  status = "primary",
-                  width = NULL,
-                  shiny::selectInput(
-                    inputId = "domain",
-                    label = "Domain",
-                    choices = domain_names,
-                    selectize = FALSE,
-                    size = 10
-                  )
-                )
-              ),
-              shiny::column(
-                width = 9,
-                shinydashboard::box(
-                  title = "Summary Information",
-                  solidHeader = TRUE,
-                  status = "primary",
-                  width = NULL,
-                  shiny::fluidRow(
-                    shiny::column(
-                      width = 3,
-                      shinydashboard::valueBoxOutput("nSubj", width = "100%")
-                    ),
-                    shiny::column(
-                      width = 4,
-                      DT::DTOutput("domainLabels")
-                    ),
-                    shiny::column(
-                      width = 4,
-                      DT::DTOutput("uniqueValues")
-                    )
-                  )
-
-                )
-              )
-            ),
-            shiny::fluidRow(
-              shiny::column(
-                width = 12,
-                shinydashboard::box(
-                  title = "Data View",
-                  solidHeader = TRUE,
-                  status = "primary",
-                  width = NULL,
-                  DT::DTOutput("domainData")
-                )
-              )
-            )
-          ),
-
-          shinydashboard::tabItem(tabName = "subjects",
-                                  shiny::fluidRow(
-                                    shiny::column(
-                                      width = 3,
-                                      shinydashboard::box(
-                                        width = NULL,
-                                        status = "primary",
-                                        solidHeader = TRUE,
-                                        title = "Subject ID",
-                                        shiny::textInput("usubjid", "USUBJID", value = "")
-                                      )
-                                    ),
-                                    shiny::column(
-                                      width = 9,
-                                      shinydashboard::box(
-                                        width = NULL,
-                                        title = "Domains to Display",
-                                        status = "primary",
-                                        solidHeader = TRUE,
-                                        shiny::checkboxGroupInput(
-                                          "subjectDomains",
-                                          "Domains to Display",
-                                          choices = domain_names,
-                                          selected = domain_names,
-                                          inline = TRUE
-                                        )
-                                      )
-                                    )
-                                  ),
-                                  shiny::uiOutput("subjectUI")
-          )
-        )
-      )
-    )
-
-  server <- function(input, output) {
-
-    rV <- shiny::reactiveValues()
-
-    observeEvent(input$domain, {
-      rV$domainData <- .src_list[[input$domain]]
-      rV$domainLabels <-
-        .src_list$mrgda_labels %>%
-        dplyr::filter(DOMAIN == input$domain) %>%
-        dplyr::select(COLUMN_NAME, COLUMN_LABEL)
-    })
-
-    output$domainData <- DT::renderDT({
-      rV$domainData %>%
-        DT::datatable(
-          rownames = FALSE,
-          filter = "top",
-          class = 'compact cell-border stripe',
-          options = list(
-            pageLength = 10,
-            scrollX = TRUE
-          ),
-        )
-    })
-
-
-    output$domainLabels <- DT::renderDT(
-      rV$domainLabels %>%
-        DT::datatable(
-          rownames = FALSE,
-          selection = "single",
-          filter = "top",
-          class = 'compact cell-border stripe',
-          options = list(
-            pageLength = 10,
-            sorting = FALSE
-          )
-        )
-    )
-
-
-    output$nSubj <- shinydashboard::renderValueBox({
-      shinydashboard::valueBox(
-        subtitle = "N Subjects",
-        value = length(unique(.src_list[[input$domain]]$USUBJID)),
-        icon = shiny::icon("people-group"),
-      )
-    })
-
-    output$uniqueValues <- DT::renderDT({
-      req(input$domainLabels_rows_selected)
-      table(rV$domainData[[rV$domainLabels$COLUMN_NAME[input$domainLabels_rows_selected]]]) %>%
-        as.data.frame() %>%
-        dplyr::rename(Value = Var1, N = Freq) %>%
-        dplyr::arrange(-N) %>%
-        DT::datatable(
-          rownames = FALSE,
-          selection = "none",
-
-          class = 'compact cell-border stripe',
-          options = list(
-            pageLength = 10,
-            sorting = FALSE,
-            searching = FALSE
-          )
-        )
-    })
-
-    observeEvent(list(input$usubjid, input$subjectDomains), {
-
-      req(input$usubjid)
-
-      rV$subject_ui <- tagList()
-
-      for(i in domain_names){
-
-        if(!(i %in% input$subjectDomains)){
-          next
-        }
-
-        rV$subject_ui <- shiny::tagAppendChild(
-          rV$subject_ui,
-          shinydashboard::box(
-            width = 12,
-            title = i,
-            solidHeader = TRUE,
-            status = "primary",
-            try(
-              .src_list[[i]] %>%
-                dplyr::filter(USUBJID == input$usubjid) %>%
-                DT::datatable(
-                  rownames = FALSE,
-                  filter = "top",
-                  class = 'compact cell-border stripe',
-                  options = list(
-                    pageLength = 5,
-                    scrollX = TRUE
-                  ),
-                )
-            )
-          )
-        )
-
-
-      }
-
-    })
-
-    output$subjectUI <- shiny::renderUI({
-      rV$subject_ui
-    })
-
-
+src_viz <- function(.src_list) {
+  # Error handling for input
+  if (!is.list(.src_list)) {
+    stop("The input must be a list of data frames.")
   }
 
-  shiny::shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
+  if (!all(sapply(.src_list, is.data.frame))) {
+    stop("All elements of the list must be data frames.")
+  }
 
+  .src_list <- .src_list[!grepl("mrgda", names(.src_list), fixed = TRUE)]
+
+  # Define color by columns
+  color_cols <- c("USUBJID")
+
+  # Define the UI
+  ui <- shinydashboard::dashboardPage(
+    shinydashboard::dashboardHeader(title = "Data Viewer"),
+    shinydashboard::dashboardSidebar(
+      shiny::selectInput("select_df", "Domain(s) View:",
+                         choices = names(.src_list),
+                         multiple=TRUE,
+                         selected = names(.src_list)[1]),
+      shiny::textInput("usubjid_filter", "USUBJID Filter:"),
+      shiny::selectInput("color_by", "Color By:", choices = color_cols)
+    ),
+    shinydashboard::dashboardBody(shiny::uiOutput("dfDisplay"))
+  )
+
+  # Define server logic
+  server <- function(input, output) {
+    # Shiny module UI
+    dfSelectionUI <- function(id) {
+      ns <- shiny::NS(id)
+      shinydashboard::box(
+        solidHeader = TRUE,
+        status = "primary",
+        DT::DTOutput(ns("table")),
+        title = id,
+        collapsible = TRUE,
+        width = 12
+      )
+    }
+
+    # Shiny module server
+    dfSelectionServer <-
+      function(id, df, usubjid_filter, color_by) {
+        shiny::moduleServer(id, function(input, output, session) {
+          ns <- session$ns
+
+          output$table <- DT::renderDT({
+            tryCatch({
+              data <- df()
+
+              # Filter data
+              if (usubjid_filter() != "" && "USUBJID" %in% names(data)) {
+                data <- dplyr::filter(data, USUBJID == usubjid_filter())
+              }
+
+              # If 'color_by' column is selected and exists in the data frame, color the rows
+              if (!is.null(color_by()) && color_by() %in% names(data)) {
+
+                colorIndex <- which(names(data) == color_by()) - 1
+
+                tableOpts = list(
+                  scrollX = TRUE,
+                  rowCallback = DT::JS(
+                    "function(row, data) {",
+                    "if (typeof window.lastCOLOR === 'undefined') {",
+                    "window.lastCOLOR = null;",
+                    "window.colorFlag = false;",
+                    "}",
+                    paste0("if (window.lastCOLOR !== data[", colorIndex, "]) {"),
+                    "window.colorFlag = !window.colorFlag;",
+                    paste0("window.lastCOLOR = data[", colorIndex, "];"),
+                    "}",
+                    "if(window.colorFlag) {",
+                    "$(row).css('background-color', '#FFFFFF');",
+                    "} else {",
+                    "$(row).css('background-color', '#DDDDDD');",
+                    "}",
+                    "}"
+                  )
+                )
+              } else {
+                tableOpts = list(scrollX = TRUE)
+              }
+
+              # Retrieve labels and modify column names
+              labels <-
+                sapply(colnames(data), function(col)
+                  attr(data[[col]], "label"))
+              names_with_labels <-
+                paste("<b>",
+                      colnames(data),
+                      "</b><br>",
+                      "<i>",
+                      labels,
+                      "</i>",
+                      sep = "")
+              names(data) <- names_with_labels
+
+              # Return the data table
+              DT::datatable(
+                data,
+                rownames = FALSE,
+                escape = FALSE,
+                selection = "single",
+                class = 'compact cell-border',
+                options = tableOpts
+              )
+
+            }, error = function(e) {
+              shiny::showNotification(
+                paste(
+                  "Error: An error occurred while processing the data frame.",
+                  e$message
+                ),
+                type = "error"
+              )
+              stop(e)
+            })
+          })
+        })
+      }
+
+    output$dfDisplay <- shiny::renderUI({
+      df_names <- input$select_df
+      shiny::tagList(lapply(df_names, dfSelectionUI))
+    })
+
+    observe({
+      df_names <- input$select_df
+      lapply(df_names, function(df_name) {
+        dfSelectionServer(
+          df_name,
+          reactive(.src_list[[df_name]]),
+          reactive(input$usubjid_filter),
+          reactive(input$color_by)
+        )
+      })
+    })
+  }
+
+  shiny::shinyApp(ui = ui, server = server, options = list(launch.browser=TRUE))
 }
