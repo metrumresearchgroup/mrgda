6ca85e1949067287ef4ada9f653fe0f1f8c57e94
michaelm <michaelm@metrumrg.com>
2023-06-26 11:41:26 -0400

    Delete view_sdtm_domains and view_src_dir_summary

--- tests/testthat/test-view-sdtm-domains.R
+++
@@ -1,6 +0,0 @@
-test_that("view_sdtm_domains returns a dataframe containing abbreviations and descriptions", {
-  df_out <- view_sdtm_domains()
-  expect_equal(df_out$DOMAIN_NAME[df_out$DOMAIN == "AE"], "Adverse Events")
-  expect_equal(df_out$DOMAIN_NAME[df_out$DOMAIN == "LB"], "Laboratory Test Results")
-  expect_equal(df_out$DOMAIN_NAME[df_out$DOMAIN == "DM"], "Demographics")
-})
