ed6b06e5d97b9cf832a734fb31a73bcd1397b9b4
Kyle Barrett <barrettk@metrumrg.com>
2023-07-26 18:27:50 -0400

    formatting updates:
     - subject columns will be grouped by color
       - this adds time for rendering. This likely scales with the size of the dataset, and may not be worth it.
     - if .subject_col is not specified, will look for ID and USUBJID, and fix both headers
     - font size improvements
     - label attribute formatting

--- R/view-src.R
+++ R/view-src.R
@@ -19,7 +19,7 @@
 #' The columns with less than 20 unique values are converted to factors. It also modifies column names to include
 #' their labels, if any. The labels are extracted from column attributes.
 #'
-#' @importFrom dplyr across
+#' @importFrom dplyr across n
 #'
 #' @export
 view_src <- function(
@@ -31,10 +31,10 @@ view_src <- function(
 
   .view <- match.arg(.view)
 
-  # dont autofit width for few number of columns
-  # Otherwise DT will not align the headers properly (known open bug)
+  # Dont autofit width for few number of columns
+  # otherwise DT will not align the headers properly (known open bug)
   autoWidth <- if(ncol(.df) <= 6) FALSE else TRUE
-  # autoWidth <- TRUE
+
 
   # Determine widths
   if(isTRUE(.full_width)){
@@ -43,17 +43,18 @@ view_src <- function(
     col_width <- "30px"
   }
 
+  base_font_size <- 9
 
+  # Table options
   tableOpts = list(
     dom = "B<lf<\"datatables-scroll\"t>ipr>",
     pageLength = 100,
     lengthMenu = c(25,100,500,1000),
-    initComplete = htmlwidgets::JS("
-    function(settings, json) {
-      $(this.api().table()).css({
-        'border': '0px'
-      });
-    }"),
+    headerCallback = DT::JS(
+      "function(thead) {",
+      glue("$(thead).css('font-size', '{base_font_size+3}pt');"),
+      "}"
+    ),
     scrolly = "15vh",
     scrollX = TRUE,
     scrollY = 300,
@@ -61,19 +62,46 @@ view_src <- function(
     colReorder = TRUE,
     searchHighlight = TRUE,
     autoWidth = autoWidth,
-    select = list(style = 'os', items = 'row'),
-    columnDefs = list(list(className = 'dt-center', targets = "_all"),
-                      list(width = col_width, targets = "_all"),
-                      list(type = 'natural', targets = "_all")
-    )
+    select = list(style = 'os', items = 'row')
   )
 
-  if (!is.null(.subject_col) && .subject_col %in% names(.df)) {
-    .df <- .df %>% dplyr::relocate(!!sym(.subject_col))
-    colorIndex <- which(names(.df) == .subject_col)
-    tableOpts$rowGroup <- list(dataSrc = colorIndex - 1)
-    tableOpts$fixedColumns <- list(leftColumns = colorIndex)
+  columnDefs <- list(
+    list(className = 'dt-center', targets = "_all"),
+    list(width = col_width, targets = "_all"),
+    # Controls column ordering
+    list(type = 'natural', targets = "_all")
+  )
+
+  # If no .subject_col specified, look for USUBJID and ID
+  if(is.null(.subject_col)){
+    id_cols <- grepl("^(?i)ID$|^(?i)USUBJID$", names(.df))
+    if(any(id_cols)) .subject_col <- names(.df)[id_cols]
   }
+
+  # Fix .subject_col column
+  if (!is.null(.subject_col)) {
+    stopifnot(.subject_col %in% names(.df))
+
+    .df <- .df %>% dplyr::relocate(!!!syms(.subject_col))
+    fix_col_index <- purrr::map_dbl(.subject_col, ~ grep(paste0("^",.x, "$"), names(.df)))
+    # TODO: change this to color grouping
+    # tableOpts$rowGroup <- list(dataSrc = fix_col_index - 1)
+    tableOpts$fixedColumns <- list(leftColumns = max(fix_col_index))
+
+    # Add color column
+    .df <- .df %>%
+      dplyr::group_by(dplyr::across(all_of(.subject_col))) %>%
+      dplyr::mutate(group_id = dplyr::cur_group_id()) %>%
+      dplyr::mutate(color = ifelse(group_id %% 2 == 1, "white", "#e2e2e2")) %>%
+      dplyr::ungroup() %>% dplyr::select(-group_id)
+
+    # Hides color column in output
+    color_id <- grep("color", names(.df)) - 1
+    columnDefs <- c(columnDefs, list(list(targets = color_id, visible = F)))
+  }
+
+  # Assign columnDefs to table options
+  tableOpts$columnDefs <- columnDefs
 
   # Convert columns with less than 20 unique values to factors
   .df <- purrr::map_dfr(.df, ~ {
@@ -87,9 +115,16 @@ view_src <- function(
   # Format headers as bold, and include label attributes (if any)
   labels <- purrr::map(colnames(.df), ~ attr(.df[[.x]], "label"))
   names_with_labels <- purrr::map2_chr(colnames(.df), labels, ~{
-    sub_txt <- if(!is.null(.y)) paste0("<br>", "<i>", .y, "</i>") else ""
+    sub_txt <- if(!is.null(.y)){
+      .y <- paste0("(", .y, ")")
+      paste0("<br>", glue("<i><span style='color: #8A8B8C; font-size: {base_font_size+1.5}pt'>"), .y, "</i></span> ")
+    }else{
+      ""
+    }
     paste0("<i>", .x, "</b>", sub_txt)
   })
+
+
 
   # Return the .df table
   .df_view <- DT::datatable(
@@ -105,9 +140,12 @@ view_src <- function(
     plugins = 'natural',
     options = tableOpts
   ) %>%
-    DT::formatStyle(0:ncol(.df), target = "cell", border = '0.5px solid #ddd', padding= "1px") %>%
+    DT::formatStyle(0:ncol(.df), target = "cell", border = '0.5px solid #bbbbbb', padding= "1px") %>%
     DT::formatStyle(0, target= 'row',lineHeight='85%') %>%
-    DT::formatStyle(columns = colnames(.df), fontSize = '80%')
+    DT::formatStyle(0:ncol(.df), fontSize = paste0(base_font_size, "pt")) %>%
+    DT::formatStyle(0:ncol(.df), "color",
+                    backgroundColor = DT::styleEqual(sort(unique(.df$color)), sort(unique(.df$color)))
+    )
 
 
   if(.view == "window"){
--- tests/testthat/test-view-src.R
+++ tests/testthat/test-view-src.R
@@ -1,21 +1,22 @@
 test_that("view_src correctly modifies dataframe", {
 
-  # Create a test dataframe
+  # Create a test dataframe - additional columns are for manually inspecting the view
   df <- data.frame(
     sex = sample(c("M", "F"), 50, replace = TRUE),
     age = sample(c(22,25,26,30,32,24), 50, replace = TRUE),
     Weight = round(rnorm(50, 75, 15), 1)
   ) %>%
     dplyr::mutate(
-      USUBJID1 = paste0("STUDY-1001-3053-", 1:dplyr::n()),
+      ID = 1:dplyr::n(),
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n()),
       notes = rep("this is a very long line that should probably be wrapped", dplyr::n())
       ) %>%
-    dplyr::relocate(USUBJID1)
+    dplyr::relocate(ID, USUBJID)
 
-  attr(df$USUBJID1, "label") <- "Subject"
+  attr(df$USUBJID, "label") <- "Subject"
 
   # Test the function on the test dataframe
-  result <- view_src(df, "USUBJID1", .view = "viewer")
+  result <- view_src(df, "USUBJID", .view = "viewer")
 
   # Check that the output is a datatables object
   expect_true(inherits(result, "datatables"))
@@ -24,10 +25,10 @@ test_that("view_src correctly modifies dataframe", {
   expect_equal(nrow(result$x$data), nrow(df))
 
   # Check that the output has the correct number of columns
-  expect_equal(ncol(result$x$data), ncol(df))
+  expect_equal(ncol(result$x$data %>% dplyr::select(-color)), ncol(df))
 
   # Check that columns with less than 20 unique values are converted to factors
-  expect_true(is.factor(result$x$data$`<b>USUBJID1</b><br><i>Subject</i>`))
+  expect_true(is.factor(result$x$data$sex))
 
 })
 
