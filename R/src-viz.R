d0dc9764dfd9cfff16494fc6e49c2d81fa620fcc
Kyle Barrett <barrettk@metrumrg.com>
2023-07-27 17:14:56 -0400

    src_viz updates based on v() updates
     - bug fixes
     - infer subject column if not specified
     - create the correct global filter depending on subject column
     - new tests

--- R/src-viz.R
+++ R/src-viz.R
@@ -3,7 +3,7 @@
 #' This function creates a Shiny App for visualizing source data. It receives a list of data frames, each representing a different domain, and
 #' produces a tabbed visualizer where each tab corresponds to a domain. A global filter is provided to filter the data by a subject column.
 #'
-#' @param .src_list A list of data frames. Each data frame represents a domain that should be visualized. The list names should be the domain names.
+#' @param .src_list A named list of data frames. Each data frame represents a domain that should be visualized. The list names should be the domain names.
 #' @param .subject_col A string specifying the name of the subject column for filtering. Defaults to "USUBJID".
 #'
 #' @return A Shiny App for visualizing source data from various domains.
@@ -19,7 +19,7 @@
 #' }
 #'
 #' @export
-src_viz <- function(.src_list, .subject_col = "USUBJID") {
+src_viz <- function(.src_list, .subject_col = NULL) {
 
   if (!inherits(.src_list, "list")) {
     stop("The input must be a list of data frames.")
@@ -27,7 +27,26 @@ src_viz <- function(.src_list, .subject_col = "USUBJID") {
 
   .box_title <- .src_list$mrgda_src_meta$path
 
+  # Ensure list elements are named
+  if(is.null(names(.src_list))){
+    names(.src_list) <- paste("Dataframe", seq(length(.src_list)))
+  }
+
+  # Filter out mrgda specific dataframe
   .src_list <- .src_list[!grepl("mrgda", names(.src_list), fixed = TRUE)]
+
+  # Determine .subject_col if not specified
+  if(is.null(.subject_col)){
+    .subject_col <- check_subject_col(.src_list)
+  }else{
+    # Make sure .subject_col is valid
+    if(!any(purrr::map_lgl(.src_list, ~{.subject_col %in% names(.x)}))){
+      abort(glue(".subject_col ({.subject_col}) is not present in any dataframe"))
+    }
+  }
+
+  # Create global filter UI
+  global_filter_ui <- create_global_filter(.subject_col)
 
   ui <-
     shinydashboard::dashboardPage(
@@ -43,11 +62,7 @@ src_viz <- function(.src_list, .subject_col = "USUBJID") {
             shiny::column(
               width = 3,
               offset = 9,
-              shiny::textInput(
-                inputId = "subject_filter",
-                label = paste0("Global ", .subject_col, " Filter"),
-                width = "100%"
-              )
+              global_filter_ui
             )
           ),
           # Make a tab for every domain
@@ -66,25 +81,33 @@ src_viz <- function(.src_list, .subject_col = "USUBJID") {
       )
     )
 
-  server <- function(input, output) {
+  server <- function(input, output, session) {
+
     purrr::map2(names(.src_list), .src_list, function(.domain, .df) {
+
       output[[paste0("table", .domain)]] <- DT::renderDataTable({
 
-        do_rows_filter <-
-          (.subject_col %in% names(.df)) &
-          nchar(trimws(input$subject_filter)) > 0
+        has_subject_col <- !is.null(.subject_col) && .subject_col %in% names(.df)
+        do_rows_filter <- has_subject_col && nchar(trimws(input$subject_filter)) > 0
 
         if(do_rows_filter){
-
-          rows_keep <-
-            trimws(as.character(.df[[.subject_col]])) == trimws(input$subject_filter)
-
-          .df <- .df[rows_keep, ]
+          .df_filter <- .df %>% dplyr::filter(!!sym(.subject_col) == trimws(input$subject_filter))
+        }else{
+          .df_filter <- .df
         }
 
-        view_src(.df, .subject_col, .view = "viewer")
+        # Allows for some dataframes to not have .subject_col
+        if(has_subject_col){
+          v(.df_filter, .subject_col)
+        }else{
+          v(.df, .subject_col = NULL)
+        }
+
+
       })
     })
+
+    session$onSessionEnded(shiny::stopApp)
   }
 
   shiny::shinyApp(
@@ -92,4 +115,59 @@ src_viz <- function(.src_list, .subject_col = "USUBJID") {
     server = server,
     options = list(launch.browser = TRUE)
   )
+}
+
+
+#' Search for ID columns across list of dataframes
+#'
+#' @inheritParams src_viz
+#' @param id_cols vector of id columns to search for
+#'
+#' @keywords internal
+check_subject_col <- function(.src_list, id_cols = c("ID", "USUBJID")){
+  # Look for USUBJID and ID columns across datasets
+  id_col_df <- purrr::map_dfr(.src_list, function(df){
+    purrr::map_lgl(id_cols, function(id_col){
+      any(grepl(glue("^(?i){id_col}$"), names(df)))
+    }) %>% stats::setNames(id_cols)
+  }) %>% dplyr::mutate(dataset = names(.src_list)) %>%
+    tidyr::pivot_longer(all_of(id_cols), names_to = "id_col", values_to = "present")
+
+
+  if(!any(id_col_df$present)){
+    # No global filter if no id_cols are found
+    subject_col <- NULL
+  }else{
+    # Use the id_col with the most occurrences across the datasets
+    id_count <- id_col_df %>% dplyr::count(id_col, present)
+    subject_col <- id_count$id_col[id_count$n == max(id_count$n)]
+    # Use USUBJID if they are the same frequency
+    if(length(subject_col) > 1){
+      subject_col <- "USUBJID"
+    }
+  }
+
+  return(subject_col)
+}
+
+
+#' Create global filter
+#'
+#' @inheritParams src_viz
+#'
+#' @keywords internal
+create_global_filter <- function(.subject_col){
+  if(!is.null(.subject_col)){
+    global_filter_ui <-
+      shiny::textInput(
+        inputId = "subject_filter",
+        label = paste0("Global ", .subject_col, " Filter"),
+        width = "100%"
+      )
+
+  }else{
+    global_filter_ui <- htmltools::div()
+  }
+
+  return(global_filter_ui)
 }
--- tests/testthat/test-src-viz.R
+++ tests/testthat/test-src-viz.R
@@ -6,7 +6,15 @@ test_that("src_viz input validation", {
 
 test_that("src_viz app structure", {
   # Prepare a proper input list
-  df_list <- list(a = data.frame(x = 1:5), b = data.frame(y = 6:10))
+  df_list <- list(
+    a = data.frame(x = 1:5) %>% dplyr::mutate(
+      ID = 1:n(),
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    ),
+    b = data.frame(y = 6:10) %>% dplyr::mutate(
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    )
+  )
 
   # Run the function and get the UI
   app <- src_viz(df_list)
@@ -16,3 +24,55 @@ test_that("src_viz app structure", {
 
 })
 
+
+test_that("check_subject_col determines the correct column", {
+
+  # `USUBJID` is most common ID column
+  df_list1 <- list(
+    a = data.frame(x = 1:5) %>% dplyr::mutate(
+      ID = 1:n(),
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    ),
+    b = data.frame(y = 6:10) %>% dplyr::mutate(
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    )
+  )
+
+  # `ID` is most common ID column
+  df_list2 <- list(
+    a = data.frame(x = 1:5) %>% dplyr::mutate(
+      ID = 1:n(),
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    ),
+    b = data.frame(y = 6:10) %>% dplyr::mutate(
+      ID = 1:n()
+    )
+  )
+
+  # USUBJID and ID are both present, use USUBJID
+  df_list3 <- list(
+    a = data.frame(x = 1:5) %>% dplyr::mutate(
+      ID = 1:n(),
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    ),
+    b = data.frame(y = 6:10) %>% dplyr::mutate(
+      ID = 1:n(),
+      USUBJID = paste0("STUDY-1001-3053-", 1:dplyr::n())
+    )
+  )
+
+  expect_equal(check_subject_col(df_list1), "USUBJID")
+  expect_equal(check_subject_col(df_list2), "ID")
+  expect_equal(check_subject_col(df_list3), "USUBJID")
+})
+
+
+test_that("create_global_filter creates the correct UI", {
+
+  filter_ui <- create_global_filter(.subject_col = NULL)
+  expect_equal(filter_ui, htmltools::div())
+
+  id_col <- "USUBJID"
+  filter_ui <- create_global_filter(.subject_col = id_col)
+  expect_true(grepl(id_col, as.character(filter_ui)))
+})
