24f136310e6c3e55c13134e741aff680ea69b34e
Kyle Barrett <barrettk@metrumrg.com>
2023-08-03 12:46:21 -0400

    add .rs.invokeShinyPaneViewer to global variables

--- R/AAA.R
+++ R/AAA.R
@@ -4,3 +4,6 @@ utils::globalVariables(c("."))
 # tidyselect::where isnt exported in earlier versions
 # this will add support for earlier cases
 utils::globalVariables("where")
+
+# needed for running a shiny app in the Rstudio viewer
+utils::globalVariables(".rs.invokeShinyPaneViewer")
