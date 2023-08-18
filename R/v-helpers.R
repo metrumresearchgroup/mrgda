36ff129d609e1a82ba3fea2066a031939de33044
Kyle Barrett <barrettk@metrumrg.com>
2023-08-18 16:52:26 -0400

    add styler to pkgr and run on v-helpers

--- R/v-helpers.R
+++ R/v-helpers.R
@@ -1,4 +1,3 @@
-
 # create_v_datatable (main DT function) -----------------------------------
 
 
@@ -52,41 +51,38 @@ create_v_datatable <- function(
     .trunc_length = 20,
     .ft_size = 9,
     .scroll_y = 400,
-    .subj_contrast = FALSE
-){
-
-
+    .subj_contrast = FALSE) {
   # This shouldnt be triggered by users, but is a precautionary abort call to avoid
   # crashing an R session when attempting to render large datasets on the client side.
-  if(rlang::is_interactive() && object.size(.df) > 2.1e6){
+  if (rlang::is_interactive() && object.size(.df) > 2.1e6) {
     size <- format(as.numeric(object.size(.df)), scientific = TRUE, digits = 4)
     msg <- c(
       "x" = glue(".df object size is {size}, which must be less than 2.1e6 to render on the client side."),
-      "i"= "Use `mrgda::v(.df)` for large datasets, which renders the table using your R console"
+      "i" = "Use `mrgda::v(.df)` for large datasets, which renders the table using your R console"
     )
     cli::cli_abort(msg)
   }
 
   # Dont autofit width for few number of columns
   # otherwise DT will not align the headers properly (known open bug)
-  autoWidth <- if(ncol(.df) <= 8) FALSE else TRUE
+  autoWidth <- if (ncol(.df) <= 8) FALSE else TRUE
 
   # Set basic options
   col_width <- "1px"
   base_font_size <- .ft_size
 
   # Core table options
-  tableOpts = list(
+  tableOpts <- list(
     dom = "<'dt-wrapper'<'datatables-scroll'rt><'row-pad' lf>ip>",
     pageLength = 100,
-    lengthMenu = c(25,100,500,1000,5000),
+    lengthMenu = c(25, 100, 500, 1000, 5000),
     scrollX = TRUE,
     scrollY = .scroll_y,
-    scrollCollapse=TRUE,
+    scrollCollapse = TRUE,
     colReorder = TRUE,
     searchHighlight = TRUE,
     autoWidth = autoWidth,
-    select = list(style = 'os', items = 'row')
+    select = list(style = "os", items = "row")
   )
 
 
@@ -112,24 +108,24 @@ create_v_datatable <- function(
   )
 
   columnDefs <- list(
-    list(className = 'dt-center', targets = "_all"),
+    list(className = "dt-center", targets = "_all"),
     list(width = col_width, targets = "_all"),
     # Controls column ordering
-    list(type = 'natural', targets = "_all"),
+    list(type = "natural", targets = "_all"),
     # Show NAs (to differentiate between blank and NA)
     list(targets = "_all", render = DT::JS(
       "function(data, type, row, meta) {",
       "return data === null ? 'NA' : data;",
-      "}")
-    )
+      "}"
+    ))
   )
 
   # If no .subject_col specified, look for USUBJID and ID (choose first in appearance)
-  if(is.null(.subject_col)){
+  if (is.null(.subject_col)) {
     id_cols <- grepl("^(?i)ID$|^(?i)USUBJID$", names(.df))
-    if(any(id_cols)) .subject_col <- names(.df)[id_cols]
-    if(length(.subject_col) > 1) .subject_col <- .subject_col[1]
-  }else{
+    if (any(id_cols)) .subject_col <- names(.df)[id_cols]
+    if (length(.subject_col) > 1) .subject_col <- .subject_col[1]
+  } else {
     assertthat::assert_that(length(.subject_col) == 1)
   }
 
@@ -140,7 +136,7 @@ create_v_datatable <- function(
     stopifnot(.freeze_cols %in% names(.df))
 
     .df <- .df %>% dplyr::relocate(!!!syms(.freeze_cols))
-    fix_col_index <- purrr::map_dbl(.freeze_cols, ~ grep(paste0("^",.x, "$"), names(.df)))
+    fix_col_index <- purrr::map_dbl(.freeze_cols, ~ grep(paste0("^", .x, "$"), names(.df)))
     tableOpts$fixedColumns <- list(leftColumns = max(fix_col_index))
 
     if (!is.null(.subject_col)) {
@@ -158,11 +154,12 @@ create_v_datatable <- function(
           ft_color = ifelse(.data$group_id %% 2 == 1, "black", "black"),
           subj_border = ifelse(dplyr::row_number() == dplyr::last(dplyr::row_number()), 1, 0)
         ) %>% # was #ececec
-        dplyr::ungroup() %>% dplyr::select(-"group_id")
+        dplyr::ungroup() %>%
+        dplyr::select(-"group_id")
     }
   }
 
-  if(!("bg_color" %in% names(.df))){
+  if (!("bg_color" %in% names(.df))) {
     .df <- .df %>% dplyr::mutate(bg_color = "white", ft_color = "black")
   }
 
@@ -177,15 +174,16 @@ create_v_datatable <- function(
 
   # Format headers as bold, and include column attributes (label and class)
   names_with_labels <- format_v_headers(.df,
-                                        .font_size = base_font_size - 1,
-                                        .show_labels = .show_labels,
-                                        .wrap_labels = .wrap_labels,
-                                        .trunc_labels = .trunc_labels,
-                                        .trunc_length = .trunc_length)
+    .font_size = base_font_size - 1,
+    .show_labels = .show_labels,
+    .wrap_labels = .wrap_labels,
+    .trunc_labels = .trunc_labels,
+    .trunc_length = .trunc_length
+  )
 
   # Round numeric columns to 3 decimal places
-  if(!is.null(.digits)){
-    .df <- .df %>% dplyr::mutate(across(where(is.numeric), ~prettyNum2(.x, .digits)))
+  if (!is.null(.digits)) {
+    .df <- .df %>% dplyr::mutate(across(where(is.numeric), ~ prettyNum2(.x, .digits)))
   }
 
 
@@ -199,7 +197,7 @@ create_v_datatable <- function(
 
   # Convert columns with less than 20 unique values to factors
   .df <- purrr::map_dfr(.df, ~ {
-    if(length(unique(.x)) < 20){
+    if (length(unique(.x)) < 20) {
       as.factor(.x)
     } else {
       .x
@@ -208,11 +206,11 @@ create_v_datatable <- function(
 
 
   # User controlled table options
-  filter <- if(.show_filters) list(position = 'top', clear = FALSE) else "none"
+  filter <- if (.show_filters) list(position = "top", clear = FALSE) else "none"
 
   # Determine lineheight based on font size
   # EQ developed from 40% = 5pt & 100% = 12pt (lm(c(40, 100) ~ c(5, 12)))
-  line_height <- paste0(round(8.6*base_font_size - 2.9), "%")
+  line_height <- paste0(round(8.6 * base_font_size - 2.9), "%")
 
   # Return the .df table
   .df_view <- DT::datatable(
@@ -222,15 +220,15 @@ create_v_datatable <- function(
     escape = FALSE,
     filter = filter,
     selection = "none",
-    class = 'cell-border hover order-column nowrap',
+    class = "cell-border hover order-column nowrap",
     extensions = c("RowGroup", "ColReorder", "FixedColumns"),
-    plugins = 'natural',
+    plugins = "natural",
     options = tableOpts
   ) %>%
     # Column Borders
-    DT::formatStyle(0:ncol(.df), target = "cell", border = '1px solid #bbbbbb', padding = "2px") %>%
+    DT::formatStyle(0:ncol(.df), target = "cell", border = "1px solid #bbbbbb", padding = "2px") %>%
     # Line Height
-    DT::formatStyle(0, target = 'row', lineHeight = line_height) %>%
+    DT::formatStyle(0, target = "row", lineHeight = line_height) %>%
     # Font Size
     DT::formatStyle(0:ncol(.df), fontSize = paste0(base_font_size, "pt")) %>%
     # Background color
@@ -246,11 +244,12 @@ create_v_datatable <- function(
     suppressWarnings()
 
   # Column Borders for fixed columns
-  if(!is.null(tableOpts$fixedColumns)){
+  if (!is.null(tableOpts$fixedColumns)) {
     .df_view <- .df_view %>%
       DT::formatStyle(
-        tableOpts$fixedColumns$leftColumns, target = "cell",
-        `border-right` = '2px solid #007319', padding = "1px"
+        tableOpts$fixedColumns$leftColumns,
+        target = "cell",
+        `border-right` = "2px solid #007319", padding = "1px"
       )
   }
 
@@ -279,21 +278,20 @@ format_v_headers <- function(
     .show_labels = TRUE,
     .wrap_labels = FALSE,
     .trunc_labels = TRUE,
-    .trunc_length = 20
-){
+    .trunc_length = 20) {
   col_attributes <- purrr::map(colnames(.df), ~ {
     list(col_lbl = attr(.df[[.x]], "label"), col_class = readr::guess_parser(as.character(.df[[.x]])))
   })
 
-  names_with_labels <- purrr::map2_chr(colnames(.df), col_attributes, function(col_name, col_attr){
+  names_with_labels <- purrr::map2_chr(colnames(.df), col_attributes, function(col_name, col_attr) {
     # Label attributes
-    lbl_sub_txt <- if(!is.null(col_attr$col_lbl)){
+    lbl_sub_txt <- if (!is.null(col_attr$col_lbl)) {
       col_lbl <- paste0(col_attr$col_lbl)
-      if(isTRUE(.trunc_labels)){
+      if (isTRUE(.trunc_labels)) {
         assertthat::is.number(.trunc_length)
         col_lbl <- col_lbl %>% stringr::str_trunc(.trunc_length)
       }
-      if(isTRUE(.wrap_labels)){
+      if (isTRUE(.wrap_labels)) {
         col_lbl <- col_lbl %>%
           gsub(" ", "<br>", ., fixed = TRUE) %>%
           gsub("_", "<br>", ., fixed = TRUE)
@@ -301,7 +299,7 @@ format_v_headers <- function(
       lbl_style <- glue("<span style='color: #8A8B8C; font-size: {.font_size-0.25}pt;
                         display:block; margin-top:1px; line-height:95%;'>")
       paste0(lbl_style, col_lbl, "</span>")
-    }else{
+    } else {
       ""
     }
 
@@ -312,7 +310,9 @@ format_v_headers <- function(
       col_attr$col_class == "datetime" ~ "<dttm>",
       col_attr$col_class == "logical" ~ "<lgl>",
       TRUE ~ paste0("<", col_attr$col_class, ">")
-    ) %>% gsub(">", "&gt;", .) %>% gsub("<", "&lt;", .)
+    ) %>%
+      gsub(">", "&gt;", .) %>%
+      gsub("<", "&lt;", .)
 
     # Define color based on class
     class_color <- dplyr::case_when(
@@ -330,12 +330,11 @@ format_v_headers <- function(
     # Create overall header
     col_name_style <- glue("<h4 style = 'font-size: {.font_size+1}pt; line-height:0%; font-weight: bold;'>")
     col_name <- paste0(col_name_style, col_name, "</h4>")
-    final_label <- if(isTRUE(.show_labels)){
+    final_label <- if (isTRUE(.show_labels)) {
       paste0(col_name, lbl_sub_txt, class_sub_text)
-    }else{
+    } else {
       paste0(col_name, class_sub_text)
     }
-
   })
 
   return(names_with_labels)
@@ -369,16 +368,16 @@ prettyNum2 <- function(.x, .digits = 3) {
 #' @return list of dataframes
 #'
 #' @keywords internal
-setup_v_list <- function(.df_list, .subject_col){
-  if(!inherits(.df_list, "list")){
-    if(!(inherits(.df_list, "data.frame") || inherits(.df_list, "tbl_df"))){
+setup_v_list <- function(.df_list, .subject_col) {
+  if (!inherits(.df_list, "list")) {
+    if (!(inherits(.df_list, "data.frame") || inherits(.df_list, "tbl_df"))) {
       cli::cli_abort(".df_list must be a list, data.frame, or tibble")
     }
     .df_list <- list(.df_list)
   }
 
   # Ensure list elements are named
-  if(is.null(names(.df_list))){
+  if (is.null(names(.df_list))) {
     names(.df_list) <- paste("Dataframe", seq(length(.df_list)))
   }
 
@@ -414,34 +413,36 @@ setup_v_list <- function(.df_list, .subject_col){
 gather_v_cols <- function(
     .df_list,
     .subject_col = NULL,
-    .id_cols = c("USUBJID", "ID")
-){
-
-  if(!is.null(.subject_col)){
+    .id_cols = c("USUBJID", "ID")) {
+  if (!is.null(.subject_col)) {
     # Make sure .subject_col is valid if user specified
-    if(!any(purrr::map_lgl(.df_list, ~{.subject_col %in% colnames(.x)}))){
+    if (!any(purrr::map_lgl(.df_list, ~ {
+      .subject_col %in% colnames(.x)
+    }))) {
       abort(glue(".subject_col ({.subject_col}) is not present in any dataframe"))
     }
     subject_col <- .subject_col
-  }else{
+  } else {
     # Look for USUBJID and ID columns across datasets
-    id_col_df <- purrr::map_dfr(.df_list, function(df){
-      purrr::map_lgl(.id_cols, function(id_col){
+    id_col_df <- purrr::map_dfr(.df_list, function(df) {
+      purrr::map_lgl(.id_cols, function(id_col) {
         any(grepl(glue("^(?i){id_col}$"), names(df)))
       }) %>% stats::setNames(.id_cols)
-    }) %>% dplyr::mutate(dataset = names(.df_list)) %>%
+    }) %>%
+      dplyr::mutate(dataset = names(.df_list)) %>%
       tidyr::pivot_longer(all_of(.id_cols), names_to = "id_col", values_to = "present")
 
-    if(!any(id_col_df$present)){
+    if (!any(id_col_df$present)) {
       # Set to NULL if none are found
       subject_col <- NULL
-    }else{
+    } else {
       # Use the id_col with the most occurrences across the datasets
-      id_count <- id_col_df %>% dplyr::filter(.data$present) %>%
+      id_count <- id_col_df %>%
+        dplyr::filter(.data$present) %>%
         dplyr::count(.data$id_col, .data$present)
       subject_col <- id_count$id_col[id_count$n == max(id_count$n)]
       # Use USUBJID if they are the same frequency
-      if(length(subject_col) > 1){
+      if (length(subject_col) > 1) {
         subject_col <- .id_cols[1]
       }
     }
@@ -457,8 +458,8 @@ gather_v_cols <- function(
     dplyr::arrange(dplyr::desc(.data$n_domain))
 
 
-  if(!is.null(subject_col)){
-    cli::cli_alert_info(glue::glue("Detected Subject Column: " , subject_col))
+  if (!is.null(subject_col)) {
+    cli::cli_alert_info(glue::glue("Detected Subject Column: ", subject_col))
   }
 
   return(
@@ -480,8 +481,8 @@ gather_v_cols <- function(
 #' @returns tibble
 find_df_with_col <- function(.col_name, .df_list) {
   df_with_col <- character(0)
-  for(i in seq_along(.df_list)){
-    if(.col_name %in% colnames(.df_list[[i]])){
+  for (i in seq_along(.df_list)) {
+    if (.col_name %in% colnames(.df_list[[i]])) {
       df_with_col <- c(df_with_col, names(.df_list)[i])
     }
   }
@@ -489,10 +490,10 @@ find_df_with_col <- function(.col_name, .df_list) {
   max_elements <- 2 # Max domains to display
   n_domains <- length(df_with_col) # N domains found
 
-  if(n_domains > max_elements){
+  if (n_domains > max_elements) {
     sub_txt <- paste0(df_with_col[1:max_elements], collapse = ", ")
     sub_txt <- paste(sub_txt, "and", n_domains - max_elements, "more")
-  }else{
+  } else {
     sub_txt <- paste0(df_with_col, collapse = ", ")
   }
 
@@ -515,26 +516,27 @@ find_df_with_col <- function(.col_name, .df_list) {
 #' @returns a `shiny.tag` object
 #'
 #' @keywords internal
-make_v_caption <- function(.name, .df, .subject_col = NULL, .font_size = 10){
-
+make_v_caption <- function(.name, .df, .subject_col = NULL, .font_size = 10) {
   # Calculate N subjects
-  if(!is.null(.subject_col)){
-    num_subj <- .df %>% dplyr::count(!!!syms(.subject_col)) %>% nrow()
-    n_subj_txt <- glue('(N {.subject_col}: {num_subj})')
-  }else{
+  if (!is.null(.subject_col)) {
+    num_subj <- .df %>%
+      dplyr::count(!!!syms(.subject_col)) %>%
+      nrow()
+    n_subj_txt <- glue("(N {.subject_col}: {num_subj})")
+  } else {
     num_subj <- 0
-    n_subj_txt <- 'No subjects detected'
+    n_subj_txt <- "No subjects detected"
   }
 
   # Format dataframe name
-  name_style <- glue('text-align: center; font-size:{.font_size}pt; font-weight: bold; display: block;')
+  name_style <- glue("text-align: center; font-size:{.font_size}pt; font-weight: bold; display: block;")
   name_html <- htmltools::tags$span(
     style = name_style,
     .name
   )
 
   # Format subject count
-  n_subj_style <- glue('text-align: center; font-size:{.font_size-2}pt; display: block;')
+  n_subj_style <- glue("text-align: center; font-size:{.font_size-2}pt; display: block;")
   n_subj_html <- htmltools::tags$span(
     style = n_subj_style,
     n_subj_txt
@@ -545,8 +547,10 @@ make_v_caption <- function(.name, .df, .subject_col = NULL, .font_size = 10){
   html_caption <- paste0(name_html, n_subj_html)
   html_caption <- as.character(shiny::tags$div(htmltools::HTML(html_caption)))
 
-  tibble::tibble(name = .name, n_subs = num_subj,
-                 label = caption, html_label = html_caption)
+  tibble::tibble(
+    name = .name, n_subs = num_subj,
+    label = caption, html_label = html_caption
+  )
 }
 
 
@@ -561,19 +565,20 @@ make_v_caption <- function(.name, .df, .subject_col = NULL, .font_size = 10){
 #' @returns a `shiny.tag` object
 #'
 #' @keywords internal
-create_global_filter <- function(.subject_col){
-  if(!is.null(.subject_col)){
+create_global_filter <- function(.subject_col) {
+  if (!is.null(.subject_col)) {
     global_filter_ui <-
       fluidRow(
         column(
           width = 12, style = "margin-top: 7px;",
           htmltools::tags$style("div.form-group {margin-bottom: 0px;}"),
-          shiny::textInput(inputId = "subject_filter", label = NULL,
-                           placeholder = paste(.subject_col, "Filter"))
+          shiny::textInput(
+            inputId = "subject_filter", label = NULL,
+            placeholder = paste(.subject_col, "Filter")
+          )
         )
       )
-
-  }else{
+  } else {
     global_filter_ui <- shiny::div()
   }
 
@@ -589,29 +594,27 @@ create_global_filter <- function(.subject_col){
 #' @keywords internal
 #'
 #' @examples
-#'
 #' \dontrun{
 #' filter_v_subject(df, "USUBJID", "3053-4")
 #' }
 #'
 #' @return a filtered dataframe
-filter_v_subject <- function(.df, .subject_col = NULL, .subject_filter){
+filter_v_subject <- function(.df, .subject_col = NULL, .subject_filter) {
   has_subject_col <- !is.null(.subject_col) && .subject_col %in% names(.df)
   do_rows_filter <- has_subject_col && nchar(trimws(.subject_filter)) > 0
 
-  if(do_rows_filter){
+  if (do_rows_filter) {
     data_filter <- .df %>% dplyr::filter(grepl(trimws(.subject_filter), !!sym(.subject_col)))
-  }else{
+  } else {
     data_filter <- .df
   }
 
   # Allows for some dataframes to not have .subject_col
-  if(has_subject_col){
-    if(nrow(data_filter) == 0){
+  if (has_subject_col) {
+    if (nrow(data_filter) == 0) {
       data_filter[1, .subject_col] <- "No subjects found"
     }
   }
 
   return(data_filter)
 }
-
