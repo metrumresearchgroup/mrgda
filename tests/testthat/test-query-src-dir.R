aa233637ed383a2bbf6eec47f2d55f8131a8da8a
michaelm <michaelm@metrumrg.com>
2023-10-31 10:44:26 -0400

    Add N Col to history meta data file & optimize query_src_dir

--- tests/testthat/test-query-src-dir.R
+++ tests/testthat/test-query-src-dir.R
@@ -1,13 +1,22 @@
 path <- system.file("example-sdtm", package = "mrgda")
 
 test_that("query_src_dir returns all domain and column combinations with a matching string", {
+  query_white <- query_src_dir(.src_directory = path, .string = "WHITE") %>% suppressMessages()
   query_race <- query_src_dir(.src_directory = path, .string = "RACE") %>% suppressMessages()
-  expect_identical(
-    query_race$MATCHING_ROWS[query_race$COLUMN == "LBSTNRC" & query_race$MATCH == "NEGATIVE TO TRACE"],
-    "19, 20, 38, 58, 59, 80, 81"
-  )
-  expect_false(any(query_race$DOMAIN == "ae"))
-  expect_error(query_src_dir(path, "stringnotfound") %>% suppressMessages())
+  query_the <- query_src_dir(.src_directory = path, .string = "THE") %>% suppressMessages()
+  query_subject <- query_src_dir(.src_directory = path, .string = "CDISC01.200002") %>% suppressMessages()
+
+  expect_true(query_white$DOMAIN == "dm")
+  expect_true(query_white$COLUMNS == "RACE")
+
+  expect_true(query_race$COLUMNS[1] == "LBORRES,LBSTRESC,LBSTNRC")
+  expect_true(all(query_race$MATCHING == "race"))
+  expect_true(query_race$DOMAIN[2] == "mrgda_labels (ae)")
+
+  expect_true(all(c("AETERM", "RACE", "PETEST") %in% query_the$COLUMNS))
+
+  expect_true(nrow(query_subject) == 7)
+
 })
 
 test_that("query_src_dir is not case sensitive", {
